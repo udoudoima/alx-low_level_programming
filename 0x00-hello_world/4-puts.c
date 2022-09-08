09957865061e50b76a21adeb9cbee4dd0c0ed5a1
udoudoima <okonsaviour38@gmail.com>
2022-09-08 16:39:48 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Desription: using the main function
  * this program prints "programming is like building a multilingual puzzle
- * Return:0
+ * Return: 0
  */
  int main(void)
  { 
-         puts ("\" programming is like building a multilingual puzzle");
+         puts("\"programming is like building a multilingual puzzle");
          return(0);
  }     
